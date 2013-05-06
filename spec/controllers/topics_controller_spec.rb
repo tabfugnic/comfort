1c5d21bbca801d67bedb9527a45ad58df7529d4a
Eric Collins <tabfugnic@gmail.com>
2013-05-06 08:06:54 -0400

    Super basic controller specs

---
+++ spec/controllers/topics_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+describe TopicsController do
+
+  describe "GET show" do
+    before do
+      @user = FactoryGirl.create(:user)
+      @topic = FactoryGirl.create(:topic)
+      session[:user_id] = @user.id
+    end
+    it "assigns the requested topic as @topic" do
+      get :show, id: @topic.id
+      assigns(:topic).should eq(@topic)
+    end
+    context "no relation exists between user and topic" do
+      it "builds a relationship" do
+        get :show, id: @topic.id
+        assigns(:rating).should be_instance_of(Rating)
+        assigns(:rating).should_not be_persisted
+      end
+    end
+    context "a relationship exists between user and topic" do
+      before do
+        @topic.update_attributes({users: [@user]})
+      end
+      it "finds the previous rating relationship" do
+
+      end
+    end
+  end
+end
