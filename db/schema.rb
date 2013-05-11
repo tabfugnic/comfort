ebf84ccb7a18d47ab2e35c954730aa050bfa13e1
Eric Collins <tabfugnic@gmail.com>
2013-05-10 21:33:29 -0400

    I forgot, but I need to actually have the schema and the comments migration to actually use comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130505045157) do
+ActiveRecord::Schema.define(:version => 20130510130516) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  add_index "comments", ["commentable_id", "commentable_type"], :name => "index_comments_on_commentable_id_and_commentable_type"
 
   create_table "ratings", :force => true do |t|
     t.integer  "user_id"
