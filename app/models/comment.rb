499c696b7723e6be1136008917f6d6dd217e0938
Eric Collins <tabfugnic@gmail.com>
2013-05-14 21:44:06 -0400

    Comments validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,9 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :content, :user_id
+  attr_accessible :content, :user_id, :user, :commentable_type, :commentable_id
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable
 
+  validates :content, :commentable_type, :commentable_id, :user_id, presence: true
 
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Comment do
-
+  context "validations" do
+    it "must have a valid factory" do
+      FactoryGirl.build(:comment).should be_valid
+    end
+    it "must have content" do
+      FactoryGirl.build(:comment, content: nil).should_not be_valid
+    end
+    it "must have commentable type" do
+      FactoryGirl.build(:comment, commentable_type: nil).should_not be_valid
+    end
+    it "must have a commmentable id" do
+      FactoryGirl.build(:comment, commentable_id: nil).should_not be_valid
+    end
+    it "must have a user" do
+      FactoryGirl.build(:comment, user: nil).should_not be_valid
+    end
+  end
 end
