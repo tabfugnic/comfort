8a8da1b0ac73101713580b47bc63ac14711bdfb5
Eric Collins <tabfugnic@gmail.com>
2013-05-20 22:16:24 -0400

    Threaded comments on the topics page

--- features/step_definitions/comment_steps.rb
+++ features/step_definitions/comment_steps.rb
@@ -1,14 +1,30 @@
+Given /topic has a comment/ do
+  @comment = FactoryGirl.create(:comment, commentable_type: "Topic", commentable_id: @topic.id, user: @user )
+end
+
 When /comment on a topic/ do
   @comment_text = "Random text"
-  fill_in("Content", with: @comment_text)
+  fill_in("comment[content]", with: @comment_text)
+  click_on("Create Comment")
 end
 
 When /comment on a comment/ do
   @comment_text = "More text"
-  find(:css, ".comment-reply").click
-  fill_in("#new_comment", with: @comment_text)
+  find(:css, "#comment-#{@comment.id} .reply").click
+  within(:css, "#comment-#{@comment.id}") do
+    fill_in("comment[content]", with: @comment_text)
+    click_on("Create Comment")
+  end
 end
 
-Then /comment should be added/ do
-  page.should have_content @comment_text
+Then /comment should be added for the topic/ do
+  within(:css, ".comment") do
+    page.should have_content @comment_text
+  end
+end
+
+Then /comment should be added within the comment/ do
+  within(:css, "#comment-#{@comment.id}.comment") do
+    page.should have_content @comment_text
+  end
 end
--- features/topic_comments.feature
+++ features/topic_comments.feature
@@ -8,11 +8,13 @@ Feature: Topic comments
     And a topic exists
     And I am on a topic page
     When I comment on a topic
-    Then the comment should be added
+    Then the comment should be added for the topic
 
+  @javascript
   Scenario: User can comment on comments
     Given I am signed in
     And a topic exists
+    And that topic has a comment
     And I am on a topic page
     When I comment on a comment
-    Then the comment should be added
+    Then the comment should be added within the comment
